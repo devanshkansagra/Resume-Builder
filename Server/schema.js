6dab0b619cc52ba6c55b9b722de04ad9a9912bc2
Devansh Kansagra <22it423@bvmengineering.ac.in>
2024-02-24 18:55:48 +0530

    Created Server and connected to the mongodb database

---
+++ Server/schema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+const userSchema = mongoose.model('users', schema);
+module.exports = userSchema;
---
+++ Server/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
+const users = require('./schema');
+// Dotenv config
+env.config({ path: '../.env' });
+
+// Server Port
+const port = process.env.PORT;
+
+// Database
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => {
+    console.log("Database connected successfully");
+}).catch(() => {
+    console.log("Unable to connect to the database");
+})
+
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send("This is a root");
+    console.log("This is a root");
+})
+
+app.post('/signup', async (req, res) => {
+
+    console.log('Signup');
+    let { firstName, lastName, email, username, password } = req.body;
+    try {
+
+        if (!firstName || !lastName || !email || !username || !password) {
+            res.status(422).send({ message: "Details are not entered properly" });
+        }
+
+        const response = await users.findOne({ email: email });
+        if (response) {
+            res.status(403).send({ message: "Email already exists" });
+        }
+        else {
+            const user = new users({ firstName, lastName, email, username, password })
+            const userSave = user.save();
+            if (userSave) {
+                res.status(201).send({ message: "User Registered Successfully" });
+                console.log(user);
+            }
+        }
+        console.log("Signup");
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+app.post('/login', async (req, res) => {
+
+    let { email, password } = req.body;
+    try {
+
+        if (!email, !password) {
+            res.status(422).send({ message: "Details are not entered properly" });
+        }
+        else {
+            const user = await users.findOne({ email: email });
+            if (user) {
+                res.status(201).send({message: "Logged in successfully"})
+            }
+            else {
+                res.status(422).send({ message: "Invalid credentials" });
+            }
+        }
+        console.log("Login");
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server is live on port ${port}`);
+})
